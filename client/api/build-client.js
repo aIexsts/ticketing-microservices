7b06830a6dc84ef93be8ff37b067e003ce6630da
Alex <astserba@gmail.com>
2021-09-25 13:45:07 +0300

    Updated ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://silverest.xyz',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
