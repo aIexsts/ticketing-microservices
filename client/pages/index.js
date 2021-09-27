13b745c16d7bd4b12b2d759bd7ca2d2e86b5aecd
Alex <astserba@gmail.com>
2021-09-27 18:45:30 +0300

    Updated ingress

--- client/pages/index.js
+++ client/pages/index.js
@@ -33,6 +33,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
+  // const { data } = await client.get('/api/tickets');
   const { data } = [];
 
   return { tickets: data };
